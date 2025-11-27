204169886786b3a0a4b1f91d32fe855523f6392c
erkmiap <miklos.parrag@ericsson.com>
2025-11-27 13:51:24 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello 2015 Advent!!!");
+        System.out.println("Hello 2025 Advent!!!");
 //        new Day0();
     }
 }
